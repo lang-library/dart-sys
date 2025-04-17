7befcaa4b0708e24e6ad09cdcda9525af12a5643
JavaCommons Technologies <javacommons@gmail.com>
2025-04-18 01:45:34 +0900

    .

---
+++ lib/sys.dart
@@ -0,0 +1,76 @@
+import 'dart:core';
+import 'dart:convert' as convert;
+import 'dart:io' as io;
+import 'dart:typed_data';
+import 'package:path/path.dart' as path;
+
+String? getenv(String $name) {
+  return io.Platform.environment[$name];
+}
+
+void setCwd(String $path) {
+  io.Directory.current = $path;
+}
+
+String getCwd() {
+  return io.Directory.current.absolute.path;
+}
+
+String pathFullName(String $path) {
+  return path.normalize(path.absolute($path));
+}
+
+String pathDirectoryName(String $path) {
+  return path.dirname($path);
+}
+
+String pathFileName(String $path) {
+  return path.basename($path);
+}
+
+String pathBaseName(String $path) {
+  return path.basenameWithoutExtension($path);
+}
+
+List<String> pathFiles(String $path) {
+  final $dir = io.Directory(path.join($path));
+  final List<io.FileSystemEntity> $entities = $dir.listSync().toList();
+  final Iterable<io.File> $files = $entities.whereType<io.File>();
+  List<String> result = [];
+  $files.toList().forEach((x) {
+    result.add(pathFullName(x.path));
+  });
+  return result;
+}
+
+List<String> pathDirectories(String $path) {
+  final $dir = io.Directory(path.join($path));
+  final List<io.FileSystemEntity> $entities = $dir.listSync().toList();
+  final Iterable<io.Directory> $dirs = $entities.whereType<io.Directory>();
+  List<String> result = [];
+  $dirs.toList().forEach((x) {
+    result.add(pathFullName(x.path));
+  });
+  return result;
+}
+
+Uint8List readFileBytes(String $path) {
+  final $file = io.File($path);
+  return $file.readAsBytesSync();
+}
+
+String readFileString(String $path) {
+  final $file = io.File($path);
+  return $file.readAsStringSync();
+}
+
+List<String> readFileLines(String $path) {
+  final $file = io.File($path);
+  return $file.readAsLinesSync();
+}
+
+List<String> textToLines(String $s) {
+  const $splitter = convert.LineSplitter();
+  final $lines = $splitter.convert($s);
+  return $lines;
+}
---
+++ test/run01.dart
@@ -0,0 +1,8 @@
+// import 'package:output/output.dart';
+// import 'package:sys/sys.dart';
+
+void main() {
+  // var calc = new Calculator();
+  // var result = calc.addOne(123);
+  // dump(result, 'result');
+}
