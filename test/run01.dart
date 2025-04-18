f872c5fb6599cc1478a991203a893fbe1a40596f
JavaCommons Technologies <javacommons@gmail.com>
2025-04-18 16:21:49 +0900

    .

--- test/run01.dart
+++ test/run01.dart
@@ -1,8 +1,9 @@
-// import 'package:output/output.dart';
-// import 'package:sys/sys.dart';
+ import 'package:output/output.dart';
+ import 'package:sys/sys.dart' as sys;
 
 void main() {
   // var calc = new Calculator();
   // var result = calc.addOne(123);
   // dump(result, 'result');
+  dump(sys.pathBaseName('abc.xyz.exe'));
 }
